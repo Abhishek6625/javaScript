0b40977a763d1f5d74302bb8d1ccdd42a9f6521d
Abhishek kumar <6625abhi@gmail.com>
2023-03-31 23:49:12 +0530

    javaScript

---
+++ dom_js/index.js
@@ -0,0 +1,167 @@
+// #################### ID ################
+
+// let a = document;
+// console.log("document");
+// let  a = document.getElementById("demo");
+// console.log(demo);
+// let b = document.getElementsByClassName("navbar");
+// console.log(b);
+// let c = document.getElementsByClassName("services");
+// console.log(c);
+
+
+// ##################### TagName ##########
+
+/*let a = document.getElementsByTagName('nav');
+console.log(a)
+let b = document.getElementsByTagName('li')[0];
+console.log(b)
+
+let c = document.getElementsByTagName("h2")[1];
+console.log(c);
+
+let d = document.getElementsByTagName("p")[2];
+console.log(d);
+
+let x =document.getElementById('demo');
+let show =demo.getElementsByTagName("a")[4];
+console.log(show);*/
+
+
+// ################### ClassName  ################
+
+/*let a =document.getElementsByClassName('container');
+console.log(a);
+
+let b =document.getElementById("demo");
+let show = demo.getElementsByClassName("navbar");
+console.log(show);*/
+
+
+// ################## QuerySeleter & All #############
+
+/*let a = document.querySelector("#demo");
+console.log(a);
+
+let b = document.querySelector(".cussection h1");
+console.log(b);
+
+let c = document.querySelector('a[href]');
+console.log(c);
+
+let d = document.querySelectorAll("h2")[2];
+console.log(d);
+
+let e = document.querySelectorAll("#image");
+console.log(e);
+
+let f =document.querySelectorAll(".col.right h3")[0];
+console.log(f);*/
+
+
+// let doc = document.all;
+// console.log(doc);
+
+// let doc2 =document.head;
+// let doc2 = document.images[1];
+// console.log(doc2);
+
+// let doc1 =document.links;
+// let doc1 =document.doctype;
+// let doc1 =document.baseURI;
+
+// console.log(doc1);
+
+// let doc =document;
+// console.dir(doc);
+
+// let a =document.querySelector(".navbar").innerText;
+// console.log(a);
+
+// let b =document.querySelector("#demo").getAttribute('id');
+// let b1 =document.querySelector("#demo").attributes['0'].value;
+// console.log(b1)
+
+// let c =document.querySelectorAll("#demo a")[0].attributes;
+// console.log(c);
+
+// let a = document.querySelector("section h1");
+// a.innerText="Abhishek kumar"
+// console.log(a);
+
+
+// let a = document.querySelector("section");
+// a.innerHTML="<h3>Abhishek</h3>";
+// console.log(a);
+
+
+// let a = document.querySelector("section a");
+// a.setAttribute("href","www.google.com");
+// a.setAttribute("class","demo1");
+// console.log(a);
+
+// let a =document.querySelector("body");
+// a.innerHTML="<h3>Abhishek kumar</h3>"
+// a.removeAttribute=('id');
+// console.log(a);
+
+// let a = document.querySelector("nav a");
+// a.style.backgroundColor="red";
+// a.style.color="black";
+// a.style.padding="20px";
+// console.log(a);
+
+
+// let b =document.querySelectorAll(".navbar ul li a")[1];
+// b.style.backgroundColor="blue";
+// b.style.color="white";
+// console.log(b);
+
+
+//  let c =document.querySelectorAll(".navbar ul li a");
+//  c[2].style.backgroundColor="blue";
+//  c[2].style.color="white";
+//  console.log(c);
+
+// let d =document.querySelectorAll(".navbar ul li a");
+// d.forEach(function(data){
+//     data.style.backgroundColor="blue";
+//     data.style.color="white";
+// })
+//  console.log(d);
+
+
+// let a=document.querySelectorAll("section .col h3");
+ // let b=a.className="demo4";
+// a.forEach(function(data){
+//     data.style.backgroundColor="red";
+//     data.style.color="blue"
+//     data.style.padding='20px'
+// })
+// console.log(a);
+
+
+// let a=document.querySelector('section .right').classList;
+// a.add('col_2');
+// a.add('data');
+// a.remove("data");
+// a.replace("col_2","col_3")
+//  let b=a.length;
+//  let b=a.contains("right");
+// console.log(a[1]);
+// console.log(a.item(1));
+// console.log(a);
+// console.log(b);
+
+
+
+// const changeColor= function(){
+//     let a =document.querySelector(nav).classlist;
+//     a.toggle("Pandit");
+//     console.log(a);
+// }
+
+
+
+
+
